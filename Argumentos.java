9815613cbd719e26c789efaef02466061e856747
TiagoFront-end <98805333+TiagoFront-end@users.noreply.github.com>
2022-05-01 12:36:21 -0300

    Add files via upload

---
+++ Argumentos.java
@@ -0,0 +1,6 @@
+class Argumentos {
+    public static void main( String[] args){
+        // Passando  Argumento na Interpretação dos Dados//
+        System.out.println(" Você Digitou : " + args[0]);
+    }
+}
